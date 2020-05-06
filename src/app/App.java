b0980e64ede3a6d785beef6798a27a55af587e13
Stalwart <krishnanmiyer@gmail.com>
2020-05-06 09:42:32 -0700

    Added IsPalindrome number

--- src/app/App.java
+++ src/app/App.java
@@ -1,12 +1,26 @@
 package app;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Practice");
+        
+        LocalDateTime start = LocalDateTime.now();
+        Execute();        
+        LocalDateTime end = LocalDateTime.now();
 
-        ReverseInteger rev = new ReverseInteger();
+        Duration totalElapsed = Duration.between(start, end);
+        System.out.println(String.format("Time take : - %s milliseconds", totalElapsed.toMillis()));
+    }
+
+    private static void Execute() {
+        //ReverseInteger rev = new ReverseInteger();
         //int output = rev.reverse(12345);
-        int output = rev.reverseLeet(12345);
+        //int output = rev.reverseLeet(12345);
+        
+        PalindromeNumber p = new PalindromeNumber();
+        boolean output = p.isPalindrome(10);
         System.out.println(output);
     }
 }
---
+++ src/app/PalindromeNumber.java
@@ -0,0 +1,23 @@
+package app;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+        if (x < Integer.MIN_VALUE || x > Integer.MAX_VALUE || x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revX = reverseInt(x);
+        return (x == revX);
+    }
+
+    private int reverseInt(int x) {
+        int result = 0;
+        int y = x;
+        while (y !=0) {
+            int digit = y % 10;
+            y /= 10;
+            result = result * 10 + digit;    
+        }
+        return result;
+    }
+}
